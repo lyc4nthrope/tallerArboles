85230a37ae113dd2e14c23d333d1f90f6ae4c88f
Cristhian Osorio <osoriocristhian86@gmail.com>
2025-05-01 17:16:02 -0500

    .

---
+++ treeWorkshop/src/main/java/com/example/treeworkshop/TreeWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.treeworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeWorkshopApplication.class, args);
+	}
+
+}
---
+++ treeWorkshop/src/test/java/com/example/treeworkshop/TreeWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treeworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
